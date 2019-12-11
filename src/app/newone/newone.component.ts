b6e8529831168513dceb2126fc1ddfa3c2f2462a
george <yuramail222@gmail.com>
2019-12-11 16:47:58 +0200

    clicks

--- src/app/newone/newone.component.ts
+++ src/app/newone/newone.component.ts
@@ -9,11 +9,18 @@ export class NewoneComponent implements OnInit {
   message: string;
   isok = false;
   items = ['John Lenon', 'Paul McCartney', 'Ring Star', 'George Harrison'];
+  seen: boolean;
   constructor() {
     this.message = 'world ' + new Date().toDateString() + ' ' + new Date().toTimeString();
-
+    this.seen = true;
   }
 
+  onClick(event) {
+    console.log(event);
+  }
+  reverse() {
+    this.seen = !this.seen;
+  }
   ngOnInit() {
   }
 
